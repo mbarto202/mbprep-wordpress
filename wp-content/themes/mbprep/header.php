62f1b5406c882410486a97adef963b457746445b
Michael <michael.d.barto@gmail.com>
2025-07-15 11:24:22 -0400

    plugins

--- wp-content/themes/mbprep/header.php
+++ wp-content/themes/mbprep/header.php
@@ -6,6 +6,8 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- SEO Meta Description -->
+  <meta name="description" content="Follow Michael Barto's prep journey and progress updates for natural bodybuilding.">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <?php wp_head(); ?>
 </head>
@@ -19,7 +21,7 @@
         <li><a href="<?php echo home_url(); ?>">Home</a></li>
         <li><a href="<?php echo home_url('/about-me'); ?>">About</a></li>
         <li><a href="<?php echo home_url('/progress-updates'); ?>">Progress</a></li>
-
+		<li><a href="<?php echo home_url('/contact'); ?>">Contact</a></li>
       </ul>
     </nav>
   </header>
